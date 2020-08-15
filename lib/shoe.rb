248775371ca39c76a2c3cff8b698577e2a7a19cc
Marques Avery <averymarques4@gmail.com>
2020-08-15 03:29:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 # Make your shoe class here!
 class Shoe
 
-  attr_accessor :brand
+  attr_accessor :brand, :color, :size, :material
 
-  def initialize(brand, color, size, material)
+  def initialize(brand)
     @brand = brand
   end
 
